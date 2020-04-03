5706d9e0dc8b7e2f27c9b768fa61be8355836f62
alvian-art <alvian-art@github.com>
2020-04-03 16:32:32 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,22 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Alvian.ID | Computer Science',
   siteDescription:
     'Brittany Chiang is a software engineer based in Boston, MA who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications.',
   siteKeywords:
     'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+  siteUrl: 'https://alvian-art.netlify.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
+  name: 'Alvian Dwi Pramudya',
+  location: 'Sleman, Yogyakarta',
+  email: 'alvian.1928@yahoo.com',
+  github: 'https://github.com/alvian-art',
   twitterHandle: '@bchiang7',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/alvian-art',
     },
     {
       name: 'Linkedin',
@@ -28,7 +28,7 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/alvian_cyber',
     },
     {
       name: 'Twitter',
