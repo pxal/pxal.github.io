41b854acb5e9ca00b7466744d187958c8cadd65e
alvian-art <alvian-art@github.com>
2020-04-03 17:01:13 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -104,10 +104,10 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/alvian-art"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Designed &amp; Built by Alvian Dwi Pramudya</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
