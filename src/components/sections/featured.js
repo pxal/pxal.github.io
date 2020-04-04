e159511192f4f4a99104b84e08ee8585ba9d91b5
alvian-art <alvian-art@github.com>
2020-04-04 20:11:33 +0700

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -216,7 +216,7 @@ const Featured = ({ data }) => {
 
   return (
     <StyledContainer id="projects">
-      <Heading ref={revealTitle}>Some Things I&apos;ve Built</Heading>
+      <Heading ref={revealTitle}>You can find me on</Heading>
 
       <div>
         {featuredProjects &&
@@ -227,7 +227,7 @@ const Featured = ({ data }) => {
             return (
               <StyledProject key={i} ref={el => (revealProjects.current[i] = el)}>
                 <StyledContent>
-                  <StyledLabel>Featured Project</StyledLabel>
+                  <StyledLabel>My Social Media</StyledLabel>
                   <StyledProjectName>
                     {external ? (
                       <a
