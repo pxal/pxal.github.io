617bc46fdbdbaa23e890926514558b7fdae58d58
pxal <pxal@github.com>
2020-04-08 16:43:48 +0700

    Update index.js

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,8 +123,8 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/pensieve" />
+        <title>Pensieve | Alvian ID</title>
+        <link rel="canonical" href="#" />
       </Helmet>
 
       <StyledMainContainer>
